093b6625366a6c61fc3c02729d2d2e587a65f264
Usama222000 <90505506+Usama222000@users.noreply.github.com>
2022-12-16 04:05:19 +0500

    Add files via upload

--- slack.groovy
+++ slack.groovy
@@ -1,8 +1,8 @@
-post {
-    success {
-        slackSend color: "good", message: "Build deployed successfully - JOB(${env.JOB_NAME}) BUILD#${env.BUILD_NUMBER} link:${env.BUILD_URL} ${currentBuild.getBuildCauses()[0].userName}"
-    }
-    failure {
-        slackSend failOnError:true ,color : "danger" ,message: "Build failed  - JOB(${env.JOB_NAME}) BUILD#${env.BUILD_NUMBER} link:${env.BUILD_URL} ${currentBuild.getBuildCauses()[0].userName}"
-    }
+def call(String buildResult) {
+  if ( buildResult == "SUCCESS" ) {
+    slackSend color: "good", message: "Build deployed successfully - JOB(${env.JOB_NAME}) BUILD#${env.BUILD_NUMBER} link:${env.BUILD_URL} ${currentBuild.getBuildCauses()[0].userName}"
+  }
+  else if( buildResult == "FAILURE" ) { 
+     slackSend failOnError:true ,color : "danger" ,message: "Build failed  - JOB(${env.JOB_NAME}) BUILD#${env.BUILD_NUMBER} link:${env.BUILD_URL} ${currentBuild.getBuildCauses()[0].userName}"
+  }
 }
