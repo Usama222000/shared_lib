e2d52e82faa45d65c8983e61f8487e05dd9c0e18
Usama222000 <90505506+Usama222000@users.noreply.github.com>
2022-12-16 04:42:48 +0500

    Update slack_notification.groovy

--- vars/slack_notification.groovy
+++ vars/slack_notification.groovy
@@ -1,1 +1,9 @@
 
+def call(String buildResult) {
+  if ( buildResult == "SUCCESS" ) {
+    slackSend color: "good", message: "Build deployed successfully - JOB(${env.JOB_NAME}) BUILD#${env.BUILD_NUMBER} link:${env.BUILD_URL} ${currentBuild.getBuildCauses()[0].userName}"
+  }
+  else if( buildResult == "FAILURE" ) { 
+     slackSend failOnError:true ,color : "danger" ,message: "Build failed  - JOB(${env.JOB_NAME}) BUILD#${env.BUILD_NUMBER} link:${env.BUILD_URL} ${currentBuild.getBuildCauses()[0].userName}"
+  }
+}
