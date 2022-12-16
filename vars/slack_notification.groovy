6f7a443859d39933d0e52861686110a8c3673e05
Usama222000 <90505506+Usama222000@users.noreply.github.com>
2022-12-16 05:03:04 +0500

    Update slack_notification.groovy

--- vars/slack_notification.groovy
+++ vars/slack_notification.groovy
@@ -1,14 +1,14 @@
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} ${env.BUILD_URL} was successful - ${currentBuild.getBuildCauses()[0].userName}"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} ${env.BUILD_URL} was failed - ${currentBuild.getBuildCauses()[0].userName}"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} ${env.BUILD_URL} was unstable - ${currentBuild.getBuildCauses()[0].userName}"
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} ${env.BUILD_URL} was unclear - ${currentBuild.getBuildCauses()[0].userName}"	
   }
 }
